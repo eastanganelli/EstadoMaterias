4c590612f507c7e10927435e7bbfd81d3bcb9fb8
eastanganelli <ezequielstanganelli@gmail.com>
2023-09-05 02:22:32 -0300

    feature complete

--- controllers/status/status.controller.js
+++ controllers/status/status.controller.js
@@ -1,18 +1,39 @@
-const { iri, lpii } = require('./../subjects.controller');
+const { iri, lpii } = require('./../student/subjects.controller');
 
 const StudentStatus = (req, res) => {
     const subjectFilter = Number(req.query.subject);
     const studentFilter = req.query.student;
 
-    console.log('Hello Api');
+    const streaming = false //|| process.env.STREAMING;
 
     let subject_ = null;
 
     switch(subjectFilter) {
         case 1: {
             subject_ = new iri();
-            subject_.studentData(studentFilter).then((data) => {
-                res.status(200).send(data);
+            subject_.studentData(studentFilter).then((student) => {
+                res.status(200).send(`
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 ${streaming ? 'aria-hidden="true"' : ''} class="card-title placeholder-glow"><span class="${streaming ? 'placeholder' : ''}"">${student['name']} ${student['lastname']}</span></h5>
+                            <h6 ${streaming ? 'aria-hidden="true"' : ''} class="card-subtitle mb-2 text-body-secondary placeholder-glow"><span class="${streaming ? 'placeholder' : ''}">${student['dni']}</span></h6>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <p>Notas</p>
+                                <footer class="blockquote-footer">${student['notes']['exam_i']} ${student['notes']['exam_ii']} ${student['notes']['final_tp']}</footer>
+                            </blockquote>
+                            <blockquote class="blockquote mb-0">
+                                <p>Porcentaje de Asistencia Teórica: ${student['assistance']['asist_t']['porcent']}</p>
+                                <footer class="blockquote-footer">${student['assistance']['asist_t']['table']}</footer>
+                            </blockquote>
+                            <blockquote class="blockquote mb-0">
+                                <p>Porcentaje de Asistencia Práctica: ${student['assistance']['asist_p']['porcent']}</p>
+                                <footer class="blockquote-footer">${student['assistance']['asist_p']['table']}</footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                `);
             }).catch((err) => {
                 res.status(403).send(err);
             });
@@ -20,8 +41,25 @@ const StudentStatus = (req, res) => {
         }
         case 2: {
             subject_ = new lpii();
-            subject_.studentData(studentFilter).then((data) => {
-                res.status(200).send(data);
+            subject_.studentData(studentFilter).then((student) => {
+                res.status(200).send(`
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 ${streaming ? 'aria-hidden="true"' : ''} class="card-title placeholder-glow"><span class="${streaming ? 'placeholder' : ''}"">${student['name']} ${student['lastname']}</span></h5>
+                            <h6 ${streaming ? 'aria-hidden="true"' : ''} class="card-subtitle mb-2 text-body-secondary placeholder-glow"><span class="${streaming ? 'placeholder' : ''}">${student['dni']}</span></h6>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <p>Notas</p>
+                                <footer class="blockquote-footer">${student['notes']['exam_i']} ${student['notes']['exam_ii']} ${student['notes']['final_tp']}</footer>
+                            </blockquote>
+                            <blockquote class="blockquote mb-0">
+                                <p>Porcentaje de Asistencia: ${student['asistance']['asist']['porcent']}</p>
+                                <footer class="blockquote-footer">${student['asistance']['asist']['table']}</footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                `);
             }).catch((err) => {
                 res.status(403).send(err);
             });
---
+++ controllers/student/subjects.controller.js
@@ -0,0 +1,137 @@
+const materia = require('./subject.controller');
+
+class iri extends materia {
+    constructor() {
+        super();
+        this._MySheets = materia._getSheets((String(process.env.IRI)).split(' '));
+    }
+    studentData(data) {
+        return new Promise((resolve, reject) => {
+
+            let myStudent = {
+                'status': null,
+                'assistance_t': null,
+                'assistance_p': null
+            };
+
+            this._MySheets.then(async hojas => {
+                const sheetEstado = hojas[0],
+                    sheetAsisT = hojas[1],
+                    sheetAsisP = hojas[2];
+                
+                (await sheetEstado.getRows()).find((row) => {
+                    if (row.get('DNI') == data || row.get('Mail') == data) {
+                        myStudent['status'] = row;
+                        return true;
+                    }
+                });
+
+                (await sheetAsisT.getRows()).find((row) => {
+                    if (row.get('DNI') == data || row.get('Mail') == data) {
+                        myStudent['assistance_t'] = row;
+                        return true;
+                    }
+                });
+
+                (await sheetAsisP.getRows()).find((row) => {
+                    if (row.get('DNI') == data || row.get('Mail') == data) {
+                        myStudent['assistance_p'] = row;
+                        return true;
+                    }
+                });
+
+                if (myStudent.status == null)
+                    reject(`No se encontró el estudiante`);
+
+                resolve({
+                    'name': myStudent['status'].get('Nombre'),
+                    'lastname': myStudent['status'].get('Apellido'),
+                    'dni': myStudent['status'].get('DNI'),
+                    'notes': {
+                        'exam_i':   materia._badgeExam(myStudent['status'].get('1er Parcial'), '1er Parcial'),
+                        'exam_ii':  materia._badgeExam(myStudent['status'].get('2do Parcial'), '2do Parcial'),
+                        'final_tp': materia._badgeExam(myStudent.status.get('TP Final'), 'TP Final')
+                    },
+                    'assistance': {
+                        'asist_t': {
+                            'porcent': materia._badgeAsis(myStudent['status'].get('T Asist')),
+                            'table': materia._asistanceTable(myStudent['assistance_t'])
+                        },
+                        'asist_p':{
+                            'porcent': materia._badgeAsis(myStudent['status'].get('P Asist')),
+                            'table': materia._asistanceTable(myStudent['assistance_p'])
+                        }
+                    }
+                });
+            });
+
+        });
+    }
+    InviteToOrg(Mail) {
+        return materia._GitHubInviteOrg('IRI', (new Date()).getFullYear(), Mail);
+    }
+};
+
+class lpii extends materia {
+    constructor() {
+        super();
+        this._MySheets = materia._getSheets((String(process.env.LPII)).split(' '));
+    }
+    studentData(data) {
+        return new Promise((resolve, reject) => {
+
+            let myStudent = {
+                'status': null,
+                'assistance': null
+            };
+
+            this._MySheets.then(async hojas => {
+                const sheetEstado = hojas[0],
+                    sheetAsis = hojas[1];
+                
+                (await sheetEstado.getRows()).find((row) => {
+                    if (row.get('DNI') == data || row.get('Mail') == data) {
+                        myStudent['status'] = row;
+                        return true;
+                    }
+                });
+
+                (await sheetAsis.getRows()).find((row) => {
+                    if (row.get('DNI') == data || row.get('Mail') == data) {
+                        myStudent['assistance'] = row;
+                        return true;
+                    }
+                });
+
+                if (myStudent == null)
+                    reject(`No se encontró el estudiante`);
+
+                resolve({
+                    'name': myStudent['status'].get('Nombre'),
+                    'lastname': myStudent['status'].get('Apellido'),
+                    'dni': myStudent['status'].get('DNI'),
+                    'notes': {
+                        'exam_i':   materia._badgeExam(myStudent['status'].get('1er Parcial'), '1er Parcial'),
+                        'exam_ii':  materia._badgeExam(myStudent['status'].get('2do Parcial'), '2do Parcial'),
+                        'final_tp': materia._badgeExam(myStudent['status'].get('TP Final'), 'TP Final')
+                    },
+                    'asistance': {
+                        'asist': {
+                            'porcent': materia._badgeAsis(myStudent['status'].get('Asist')),
+                            'table': materia._asistanceTable(myStudent['assistance'])
+                        }
+                    }
+                });
+            });
+
+        });
+    }
+    InviteToOrg(Mail) {
+        return materia._GitHubInviteOrg('LP2', (new Date()).getFullYear(), Mail);
+    }
+};
+
+module.exports = {
+    iri,
+    lpii
+};
